3cd96cd530de988df19e011fb5aa8764c5f44c3e
SofiaGDelgado <sofigdel@gmail.com>
2022-06-23 21:36:51 -0600

    Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LoginService } from './services/login/login.service';
+import { LoginComponent } from './component/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './component/header/header.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [LoginService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
